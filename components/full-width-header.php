b7e7e3598bea13a61c6cde0254ad38e23cfc21ea
alastair38 <alastair@alastaircox.com>
2023-10-05 10:07:38 +0100

    v0.24 + caption on full-width-header featured img

--- components/full-width-header.php
+++ components/full-width-header.php
@@ -14,7 +14,16 @@
 <header class="entry-header space-y-6">
 	
 	<?php if(is_singular() && has_post_thumbnail()):
+	echo '<figure class="relative h-full">';
 	the_post_thumbnail( 'header', ['class' => 'w-full aspect-[3/1] h-full col-start-1 row-start-1 object-cover'] );
+	
+	$caption = get_the_post_thumbnail_caption();
+	
+	if($caption):
+		echo '<span class="md:absolute md:px-3 py-1 text-tiny md:text-sm md:bottom-4 md:right-4 md:bg-white">' . $caption .'</span>';
+	endif;
+	
+	echo '</figure>';
  endif;?>
 
 <h1 class="page-title leading-snug lg:leading-normal font-black text-neutral-dark-900"><?php the_title();?></h1>
